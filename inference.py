881ca5f1d9b540429c934f8242bd3434df8a294d
doyeonkim-cubox-ai <doyeon.kim@cubox.ai>
2024-08-30 16:14:03 +0900

    FEAT : add inference

---
+++ inference.py
@@ -0,0 +1,36 @@
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+import numpy as np
+import onnx
+import onnxruntime as ort
+from PIL import Image
+
+
+def main():
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    torch.manual_seed(777)
+    if device == 'cuda':
+        torch.cuda.manual_seed_all(777)
+
+    # preprocess
+    image = Image.open("./inference_data/image1.png")
+    image = image.resize((28, 28)).convert('L')
+    img_tensor = transforms.ToTensor()(image)
+    img_tensor = img_tensor.unsqueeze(0)
+
+    model = onnx.load("./model/model.onnx")
+    session = ort.InferenceSession("./model/model.onnx")
+
+    ort_in = {session.get_inputs()[0].name: np.array(img_tensor)}
+    ort_out = session.run(None, ort_in)[0]
+
+    # postprocess
+    ort_out = ort_out.squeeze(0)
+    res = np.argmax(ort_out)
+    print(res)
+
+
+if __name__ == "__main__":
+    main()
